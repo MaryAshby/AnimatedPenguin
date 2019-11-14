6ab569373c727241aedd698c490bc58a9cdb7a2c
MaryAshby <54848877+MaryAshby@users.noreply.github.com>
2019-11-14 16:56:41 -0500

    Update aPenguins.js

--- aPenguins.js
+++ aPenguins.js
@@ -20,7 +20,12 @@ var penguinePromise= d3.json("penguins/classData.json")
 //Set up//
 //making data//
   
-var getQuizes = function(penguin)
+var getGrade = function(quiz)
+                {
+                return quiz.grade;
+                }
+
+/*var getQuizes = function(penguin)
                  {
                  return penguin.quizes.map(getGrade);
                  }
@@ -30,10 +35,7 @@ var quizArray = function(penguins)
                 return penguins.map(getQuizes);
                 }
 
-var getGrade = function(quiz)
-                {
-                return quiz.grade;
-                }
+*/
 
 
 var screen = {width: 600, height:600}
@@ -71,14 +73,15 @@ var setup = function(penguins)
            d3.select("svg")
              .attr("width",screen.width)
              .attr("height",screen.height)
-             .append("g").attr("id","graph")
+             .append("g")
+             .attr("id","graph")
              .attr("transform","translate("+margins.left+ ","+ margins.top+")");
              
 var width = screen.width - margins.left - margins.right;
 var height = screen.height - margins.top - margins.bottom;
 
 var xScale = d3.scaleLinear()
-               .domain([0,29]) //DOUBLE CHECK NUMBER OF QUIZZES!!!!//
+               .domain([0,39]) //made extra number for ascetics//
                .range([0,width])
                 
 var yScale= d3.scaleLinear()
@@ -98,37 +101,44 @@ var cScale = d3.scaleOrdinal(d3.schemeTableau10)
              .classed("axis",true);
     
            d3.select(".axis")
-             .append("g").style("font","16px times")
+             .append("g")
+             //.style("font","16px times")
              .attr("id","xAxis")
              .attr("transform","translate("+margins.left+","+(margins.top+height)+")")
              .call(xAxis)
     
            d3.select(".axis")
-             .append("g").style("font","16px times")
+             .append("g")
+             //.style("font","16px times")
              .attr("id","yAxis")
              .attr("transform","translate(25,"+margins.top+")")
              .call(yAxis)
                       
-    drawArray(penguins,xScale,yScale,cScale,0);
+    drawArray(getGrade,xScale,yScale,cScale,0);
    
            } //function ends here//
 
-var drawArray = function(penguins, xScale, yScale, cScale, position)
+var drawArray = function(getGrade, xScale, yScale, cScale, position)
           {
            var arrays = d3.select("#graph")
                           .selectAll("circle")
-                          .data(penguins[0].quizes)
-                          .transition()
+                          .datum(function(obj){return obj.quizes.map(getGrade)}) 
+                         // .transition()
                           .enter()
                           .append("circle")
                           .attr("fill", function(sweet)
                                 {
-                                 return cScale(penguins[0].quizes);
+                                 return cScale(penguins[0].quizes.grade);
                                  })
                           .attr("cx", function(num, position)
                                 {
                                  return xScale(position);
                                 })
+                          .attr("cy", function(num)
+                                {
+                                 return yScale(num);
+                                 })
+                                
                           .attr("r", 2)
           
            }
